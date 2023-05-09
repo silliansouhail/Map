454512b661ecc008adab009a098eb5ed344a4243
silliansouhail <souhailsillian3@gmail.com>
2023-05-09 11:01:05 +0100

    Map

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12952.05968661266!2d10.7103544!3d35.750438!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13020b7c4731bc19%3A0xc19a90db533c0df!2sSahline!5e0!3m2!1sen!2stn!4v1683625783752!5m2!1sen!2stn" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </header>
+    </div>
+  );
+}
+
+export default App;
